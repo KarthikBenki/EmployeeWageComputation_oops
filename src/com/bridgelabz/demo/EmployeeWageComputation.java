b26946e4630e7af28be1145288acbc7570ae23d7
karthikbenki <karthikmc007@gmail.com>
2022-02-11 20:27:14 +0530

    Added: Welome to Employee wage computation

---
+++ src/com/bridgelabz/demo/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.demo;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to Employee\r\n" + "Wage Computation\r\n" + "Program on Master Branch");
+
+    }
+
+}
